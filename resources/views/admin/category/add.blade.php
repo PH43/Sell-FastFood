edc79a047c4ca47c52f0be42b903d11d71488158
lcminh <lcminh10121999@gmail.com>
2021-09-28 11:40:03 +0700

    them sua xoa category

---
+++ resources/views/admin/category/add.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layoutadmin')
+
+@section('title')
+    <title>Danh mục</title>
+@endsection
+
+@section('content')
+
+    <div class="content-wrapper">
+    @include('admin.partials.header', [ 'key' => 'Trang thêm', 'name' => 'danh mục'])
+    <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form method="post" action="{{ route('categories.store') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label>Tên danh mục</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Nhập tên danh mục">
+                                @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Thêm</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+// trang chu admin
 Route::get('/admin', function () {
     return view('admin.home');
+});
+// trang category admin
+Route::prefix('/admin/categories')->group(function () {
+    Route::get('/',[
+        'as'=> 'categories.index',
+        'uses' => 'categoryController@index'
+    ]);
+    Route::get('/create',[
+        'as'=> 'categories.create',
+        'uses' => 'categoryController@create'
+    ]);
+    Route::post('/store',[
+        'as'=> 'categories.store',
+        'uses' => 'categoryController@store'
+    ]);
+    Route::get('/edit/{id}',[
+        'as'=> 'categories.edit',
+        'uses' => 'categoryController@edit'
+    ]);
+    Route::post('/update/{id}',[
+        'as'=> 'categories.update',
+        'uses' => 'categoryController@update'
+    ]);
+    Route::get('/delete/{id}',[
+        'as'=> 'categories.delete',
+        'uses' => 'categoryController@delete'
+    ]);
 });
