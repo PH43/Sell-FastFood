b5d6822244ab9a27a643900ff12ba0bd60bd1c7b
tâm hồ <hovantam2509@gmail.com>
2021-10-04 12:43:38 +0700

    merge layouts

--- database/migrations/2021_09_28_023840_create_categories_table.php
+++ database/migrations/2021_09_28_023840_create_categories_table.php
@@ -16,6 +16,8 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('parent_id')->default(0);
+            $table->string('slug');
             $table->timestamps();
         });
     }
